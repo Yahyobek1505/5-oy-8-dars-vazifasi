eb9a16c6d5f14b5d0c0c3518b4331804d1857d30
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-27 21:46:55 +0500

    Homework is start

---
+++ script.js
@@ -0,0 +1,80 @@
+// 6-topshiriq
+//<=================> LocalStorage ga saqlash:<=================>
+
+// let userName = "Yahyobek Urinboyev";
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   window.localStorage.setItem('fullName', userName)
+// })
+
+//  <=================> LocalStoragedan data olish:<=================> 
+
+// let userName = "Yahyobek Urinboyev";
+
+// const h1 =document.getElementById('h1')
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   let myName = window.localStorage.getItem('fullName', userName)
+//   h1.innerHTML = myName;
+//   console.log(myName);
+// })
+
+// <=================> LocalStoragedan data  o'chirish:<=================> 
+// let userName = "Yahyobek Urinboyev";
+
+// const h1 =document.getElementById('h1')
+// const deleted = "Ma'lumotlar o'chirildi"
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   let myName = window.localStorage.removeItem('fullName', userName)
+//   h1.innerHTML = deleted;
+//   console.log("O'chirildi");
+// })
+
+  //<=================>  Sessionstorage ga saqlash va olish:<=================> 
+// const h1 =document.getElementById('h1')
+// let userName = "Yahyobek Urinboyev";
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   window.sessionStorage.setItem('fullName', userName)
+//   let myName = window.sessionStorage.getItem('fullName', userName)
+//   h1.innerHTML = myName;
+//   console.log(myName);
+// })
+
+
+// <=================> Sessionstorage datani  o'chirish:<=================> 
+// let userName = "Yahyobek Urinboyev";
+
+// const h1 =document.getElementById('h1')
+// const deleted = "Ma'lumotlar o'chirildi"
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   let myName = window.sessionStorage.removeItem('fullName', userName)
+//   h1.innerHTML = deleted;
+//   console.log("O'chirildi");
+// })
+
+
+  //<=================>  Cookie ga saqlash va olish:<=================> 
+// const h1 =document.getElementById('h1')
+// let userName = "Yahyobek Urinboyev";
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   document.cookie = 'fullName', userName;
+//   let myName = window.cookie.getItem('fullName', userName)
+//   h1.innerHTML = myName;
+//   console.log(myName);
+// })
+
+// <=================> Cookie datani  o'chirish:<=================> 
+let userName = "Yahyobek Urinboyev";
+
+const h1 =document.getElementById('h1')
+const deleted = "Ma'lumotlar o'chirildi"
+
+window.addEventListener('DOMContentLoaded', function () {
+  let myName =cookie ='';
+  h1.innerHTML = deleted;
+  console.log("O'chirildi");
+})
